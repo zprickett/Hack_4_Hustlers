d9c2d67d479091c7119b047d241eba1e5e1fdf85
zprickett <zprickett@gmail.com>
2013-05-18 11:28:29 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hack4Hustlers::Application.config.secret_token = '4132bb5bf601f126c6734e8492fefd227848d78a1a12cb004bdcaccfa2e672c818c8291a9d76e455cb64d31aa79de19249e54181cf71172b48a5c42c49de24bf'
